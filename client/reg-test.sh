333e6a05b164b42654ccf63791a46c4da2eef74f
Home <328742379@qq.com>
2019-12-19 23:40:55 +0800

    编写测试脚本

--- client/reg-test.sh
+++ client/reg-test.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-CLIENT_HOME=/home/vagrant/fabric-ca/client
+export FABRIC_CA_CLIENT_HOME=/home/vagrant/fabric-ca/client
 
-export FABRIC_CA_CLIENT_HOME=${CLIENT_HOME}/ca-files
-env
+ORDERER_DIR=${FABRIC_CA_CLIENT_HOME}/crypto-config/ordererOrganizations/36sn.com
+PEER_DIR=${FABRIC_CA_CLIENT_HOME}/crypto-config/peerOrganizations/org1.36sn.com
 
-USER="orderer.36sn.com"
-PASS="616766"
-TYPE="orderer"
-AFFILIATION="org1.36sn.com"
+mkdir -p $ORDERER_DIR/ca
+mkdir -p $ORDERER_DIR/msp
+mkdir -p $ORDERER_DIR/msp/admincerts
+mkdir -p $ORDERER_DIR/msp/cacerts
+mkdir -p $ORDERER_DIR/msp/tlscacerts
+mkdir -p $ORDERER_DIR/orderers
+mkdir -p $ORDERER_DIR/tlsca
+mkdir -p $ORDERER_DIR/users
 
 set -x
-${CLIENT_HOME}/fabric-ca-client register --id.type ${TYPE} --id.name ${USER} --id.secret ${PASS} --id.affiliation ${AFFILIATION} --id.attrs '"hf.Registrar.Roles=peer,user","hf.Revoker=true"'
+${FABRIC_CA_CLIENT_HOME}/fabric-ca-client register --id.type orderer --id.name orderer.36sn.com --id.secret 123456 --id.affiliation 36sn.com --id.attrs '"hf.Registrar.Roles=orderer","hf.Revoker=true"'
 res=$?
 set +x
 if [ $res -ne 0 ]; then
@@ -21,6 +25,86 @@ fi
 echo 
 
 set -x
-${CLIENT_HOME}/fabric-ca-client enroll -d --enrollment.profile tls -u http://${USER}:${PASS}@localhost:7054 -M ${FABRIC_CA_CLIENT_HOME}/${AFFILIATION}/users/${USER}
+${FABRIC_CA_CLIENT_HOME}/fabric-ca-client enroll -u http://admin:admin@localhost:7054 -M $ORDERER_DIR/orderers/orderer.36sn.com/msp
 set +x
+echo
+
+set -x
+${FABRIC_CA_CLIENT_HOME}/fabric-ca-client register --id.type client --id.name Admin@36sn.com --id.secret 123456 --id.affiliation 36sn.com --id.attrs '"hf.Registrar.Roles=orderer,admin,client","hf.Revoker=true"'
+res=$?
+set +x
+if [ $res -ne 0 ]; then
+    echo "Failed to register ..."
+    exit 1
+fi
 echo 
+
+set -x
+${FABRIC_CA_CLIENT_HOME}/fabric-ca-client enroll -u http://admin:admin@localhost:7054 -M $ORDERER_DIR/users/Admin@36sn.com/msp
+set +x
+echo
+
+cp $ORDERER_DIR/orderers/orderer.36sn.com/msp/cacerts/*.pem $ORDERER_DIR/ca
+cp $ORDERER_DIR/orderers/orderer.36sn.com/msp/cacerts/*.pem $ORDERER_DIR/tlsca
+cp $ORDERER_DIR/orderers/orderer.36sn.com/msp/cacerts/*.pem $ORDERER_DIR/msp/cacerts
+cp $ORDERER_DIR/orderers/orderer.36sn.com/msp/cacerts/*.pem $ORDERER_DIR/msp/tlscacerts
+mkdir -p cp $ORDERER_DIR/orderers/orderer.36sn.com/tls
+cp $ORDERER_DIR/orderers/orderer.36sn.com/msp/cacerts/*.pem $ORDERER_DIR/orderers/orderer.36sn.com/tls/ca.crt
+cp $ORDERER_DIR/orderers/orderer.36sn.com/msp/signcerts/*.pem $ORDERER_DIR/orderers/orderer.36sn.com/tls/server.crt
+cp $ORDERER_DIR/orderers/orderer.36sn.com/msp/keystore/*_sk $ORDERER_DIR/orderers/orderer.36sn.com/tls/server.key
+mkdir -p cp $ORDERER_DIR/users/Admin@36sn.com/tls
+cp $ORDERER_DIR/users/Admin@36sn.com/msp/cacerts/*.pem $ORDERER_DIR/users/Admin@36sn.com/tls/ca.crt
+cp $ORDERER_DIR/users/Admin@36sn.com/msp/signcerts/*.pem $ORDERER_DIR/users/Admin@36sn.com/tls/server.crt
+cp $ORDERER_DIR/users/Admin@36sn.com/msp/keystore/*_sk $ORDERER_DIR/users/Admin@36sn.com/tls/server.key
+
+mkdir -p $PEER_DIR/ca
+mkdir -p $PEER_DIR/msp
+mkdir -p $PEER_DIR/msp/admincerts
+mkdir -p $PEER_DIR/msp/cacerts
+mkdir -p $PEER_DIR/msp/tlscacerts
+mkdir -p $PEER_DIR/peers
+mkdir -p $PEER_DIR/tlsca
+mkdir -p $PEER_DIR/users
+
+set -x
+${FABRIC_CA_CLIENT_HOME}/fabric-ca-client register --id.type peer --id.name peer0.org1.36sn.com --id.secret 123456 --id.affiliation org1.36sn.com --id.attrs '"hf.Registrar.Roles=peer,user","hf.Revoker=true"'
+res=$?
+set +x
+if [ $res -ne 0 ]; then
+    echo "Failed to register ..."
+    exit 1
+fi
+echo 
+
+set -x
+${FABRIC_CA_CLIENT_HOME}/fabric-ca-client enroll -u http://admin:admin@localhost:7054 -M $PEER_DIR/peers/peer0.org1.36sn.com/msp
+set +x
+echo
+
+set -x
+${FABRIC_CA_CLIENT_HOME}/fabric-ca-client register --id.type client --id.name Admin@org1.36sn.com --id.secret 123456 --id.affiliation org1.36sn.com --id.attrs '"hf.Registrar.Roles=peer,admin,user,client","hf.Revoker=true"'
+res=$?
+set +x
+if [ $res -ne 0 ]; then
+    echo "Failed to register ..."
+    exit 1
+fi
+echo 
+
+set -x
+${FABRIC_CA_CLIENT_HOME}/fabric-ca-client enroll -u http://admin:admin@localhost:7054 -M $PEER_DIR/users/Admin@org1.36sn.com/msp
+set +x
+echo
+
+cp $PEER_DIR/peers/peer0.org1.36sn.com/msp/cacerts/*.pem $PEER_DIR/ca
+cp $PEER_DIR/peers/peer0.org1.36sn.com/msp/cacerts/*.pem $PEER_DIR/tlsca
+cp $PEER_DIR/peers/peer0.org1.36sn.com/msp/cacerts/*.pem $PEER_DIR/msp/cacerts
+cp $PEER_DIR/peers/peer0.org1.36sn.com/msp/cacerts/*.pem $PEER_DIR/msp/tlscacerts
+mkdir -p cp $PEER_DIR/peers/peer0.org1.36sn.com/tls
+cp $PEER_DIR/peers/peer0.org1.36sn.com/msp/cacerts/*.pem $PEER_DIR/peers/peer0.org1.36sn.com/tls/ca.crt
+cp $PEER_DIR/peers/peer0.org1.36sn.com/msp/signcerts/*.pem $PEER_DIR/peers/peer0.org1.36sn.com/tls/server.crt
+cp $PEER_DIR/peers/peer0.org1.36sn.com/msp/keystore/*_sk $PEER_DIR/peers/peer0.org1.36sn.com/tls/server.key
+mkdir -p cp $PEER_DIR/users/Admin@org1.36sn.com/tls
+cp $PEER_DIR/users/Admin@org1.36sn.com/msp/cacerts/*.pem $PEER_DIR/users/Admin@org1.36sn.com/tls/ca.crt
+cp $PEER_DIR/users/Admin@org1.36sn.com/msp/signcerts/*.pem $PEER_DIR/users/Admin@org1.36sn.com/tls/server.crt
+cp $PEER_DIR/users/Admin@org1.36sn.com/msp/keystore/*_sk $PEER_DIR/users/Admin@org1.36sn.com/tls/server.key
