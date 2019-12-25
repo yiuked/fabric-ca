ce1505339cd0782e615a3d04789e2088aa6f45ba
Company <yiuked@vip.qq.com>
2019-12-25 18:20:34 +0800

    更新

---
+++ client/base.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+FABRIC_CA_CLIENT_HOME=/home/vagrant/fabric-ca/client
+
+export FABRIC_CA_CLIENT_HOME=${FABRIC_CA_CLIENT_HOME}
--- client/reg-test.sh
+++ client/reg-test.sh
@@ -6,8 +6,9 @@
 # $2 password
 # $3 type
 # $4 affiliation
+# $5 role
 function RegisterNode(){
-    NodeDir=${FABRIC_CA_CLIENT_HOME}/crypto-config/$3Organizations/$4
+    NodeDir=/home/vagrant/fabric-with-ca/crypto-config/$3Organizations/$4
     
     set -x
     rm -rf $NodeDir
@@ -20,7 +21,7 @@ function RegisterNode(){
     
 
     set -x
-    ${FABRIC_CA_CLIENT_HOME}/fabric-ca-client register --id.type $3 --id.name $1 --id.secret $2 --id.affiliation $4 --id.attrs '"hf.Registrar.Roles=peer,user","hf.Revoker=true"'
+    ${FABRIC_CA_CLIENT_HOME}/fabric-ca-client register --id.type $3 --id.name $1 --id.secret $2 --id.affiliation $4 --id.attrs '"hf.Registrar.Roles=$5,CLIENT,ADMIN","hf.Revoker=true"'
     res=$?
     set +x
     if [ $res -ne 0 ]; then
@@ -34,11 +35,16 @@ function RegisterNode(){
     set +x
     echo 
     
+    # rename
+    mv ${NodeDir}/$3s/$1/msp/cacerts/*.pem ${NodeDir}/$3s/$1/msp/cacerts/ca.$4-cert.pem
+    mkdir -p ${NodeDir}/$3s/$1/msp/tlscacerts
+    cp ${NodeDir}/$3s/$1/msp/cacerts/*.pem ${NodeDir}/$3s/$1/msp/tlscacerts/tlsca.$4-cert.pem
+    
     # 1.2.create org msp and ca and tls
     cp ${NodeDir}/$3s/$1/msp/cacerts/*.pem ${NodeDir}/msp/cacerts
-    cp ${NodeDir}/$3s/$1/msp/cacerts/*.pem ${NodeDir}/msp/tlscacerts
+    cp ${NodeDir}/$3s/$1/msp/tlscacerts/*.pem ${NodeDir}/msp/tlscacerts
     cp ${NodeDir}/$3s/$1/msp/cacerts/*.pem ${NodeDir}/ca
-    cp ${NodeDir}/$3s/$1/msp/cacerts/*.pem ${NodeDir}/tlsca
+    cp ${NodeDir}/$3s/$1/msp/tlscacerts/*.pem ${NodeDir}/tlsca
 
     # 1.2.create peer tls
     mkdir -p ${NodeDir}/$3s/$1/tls
@@ -49,7 +55,7 @@ function RegisterNode(){
 
     # 1.3.register peer admin
     set -x
-    ${FABRIC_CA_CLIENT_HOME}/fabric-ca-client register --id.type $3 --id.name Admin@$4 --id.secret $2 --id.affiliation $4 --id.attrs '"hf.Registrar.Roles=peer,user","hf.Revoker=true"'
+    ${FABRIC_CA_CLIENT_HOME}/fabric-ca-client register --id.type $3 --id.name Admin@$4 --id.secret $2 --id.affiliation $4 --id.attrs '"hf.Registrar.Roles=ADMIN","hf.Revoker=true"'
     res=$?
     set +x
     if [ $res -ne 0 ]; then
@@ -62,10 +68,15 @@ function RegisterNode(){
     ${FABRIC_CA_CLIENT_HOME}/fabric-ca-client enroll -u http://$1:$2@localhost:7054 -M ${NodeDir}/users/Admin@$4/msp
     set +x
     echo 
-
+    
+    # rename
+    mv ${NodeDir}/users/Admin@$4/msp/cacerts/*.pem ${NodeDir}/users/Admin@$4/msp/cacerts/ca.$4-cert.pem
+    mkdir -p ${NodeDir}/users/Admin@$4/msp/tlscacerts
+    cp ${NodeDir}/users/Admin@$4/msp/cacerts/*.pem ${NodeDir}/users/Admin@$4/msp/tlscacerts/tlsca.$4-cert.pem
+    
     # 1.4.create orderer user tls
     mkdir -p ${NodeDir}/users/Admin@$4/tls
-    cp ${NodeDir}/users/Admin@$4/msp/cacerts/*.pem ${NodeDir}/users/Admin@$4/tls/ca.crt
+    cp ${NodeDir}/users/Admin@$4/msp/tlscacerts/*.pem ${NodeDir}/users/Admin@$4/tls/ca.crt
     cp ${NodeDir}/users/Admin@$4/msp/keystore/*_sk ${NodeDir}/users/Admin@$4/tls/server.key
     cp ${NodeDir}/users/Admin@$4/msp/signcerts/*.pem ${NodeDir}/users/Admin@$4/tls/server.crt
     mkdir ${NodeDir}/users/Admin@$4/msp/admincerts
@@ -76,5 +87,5 @@ function RegisterNode(){
     
 }
 
-RegisterNode orderer.36sn.com abc123 orderer 36sn.com
-RegisterNode peer0.org1.36sn.com abc123 peer org1.36sn.com
+RegisterNode orderer.36sn.com abc123 orderer 36sn.com ORDERER
+RegisterNode peer0.org1.36sn.com abc123 peer org1.36sn.com PEER
